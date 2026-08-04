99aeeb2265116dedf6d3be3d3f2933b5832a4889
uwyn tarentaal <tarentaaluwyn2@gmail.com>
2026-08-05 00:01:51 +0200

    added the logic for the computer to make a random choice between Rock Paper or Scissors and added the human choice logic as well i still need to build the logic for the human and computer to play the first round

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Hello world!");
+//console.log("Hello world!");
+
+
+function getComputerChoice() {
+    let choice = ["Rock", "Paper", "Scissors"];
+    const index = Math.floor(Math.random() * choice.length);
+    return choice[index];
+}
+console.log(getComputerChoice());
+
+function getHumanChoice () {
+  let decision = ["Rock", "Paper", "Scissor"];
+  const randomChoice = Math.floor(Math.random() * decision.length);
+  return decision[randomChoice];
+}
+console.log(getHumanChoice());
+
+let userInput = prompt(`User input`);
+
+const computerScore = 0;
+const humanScore = 0;
+
+function playRound (humanChoice, computerChoice) {
+computerScore += computerChoice;
+humanScore += humanChoice;
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumnanChoice();
+}
+console.log(playRound(computerSelection, humanSelection));
