03cd5c8a6083d9bf964e5d2f7cd54fa66938776b
uwyn tarentaal <tarentaaluwyn2@gmail.com>
2026-08-08 14:53:43 +0200

    Made some changes to the ROCK PAPER SCISSORS logic and fixed the bug where the prompt popup two times and fixed some referrence errors but still need to fix some other bugs and build the logic to play 5 rounds of ROCK PAPER SCISSORS

--- script.js
+++ script.js
@@ -1,62 +1,31 @@
 //console.log("Hello world!");
 
 
-function getComputerChoice() {
-    let choice = ["Rock", "Paper", "Scissors"];
+function getComputerChoice(computer) {
+    let choice = ["ROCK", "PAPER", "SCISSORS"];
     const index = Math.floor(Math.random() * choice.length);
     return choice[index];
 }
-console.log(getComputerChoice());
+console.log(getComputerChoice("computer:"));
 
-function getHumanChoice () {
-  let decision = ["Rock", "Paper", "Scissors"];
-  const randomChoice = Math.floor(Math.random() * decision.length);
-  return decision[randomChoice];
+function gethumanChoice () {
+  let decision = ["ROCK", "PAPER", "SCISSORS"];
+const randomDecision = Math.floor(Math.random() * decision.length);
+return decision[randomDecision];
 }
-console.log(getHumanChoice());
+console.log(gethumanChoice());
 
-//let userInput = prompt(`User input`).toUpperCase();
-//while (userInput !== userInput.toUpperCase() || userInput === "");
-
-const computerScore = 0;
-const humanScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 function playRound (humanChoice, computerChoice) {
-
-let userInput = prompt(`User input`).toUpperCase() {
-  while (userInput !== userInput.toUpperCase() || userInput === "");
-}
-
-
-
 if (humanChoice === "ROCK" && computerChoice === "PAPER") {
-  computerScore++
-console.log(`You lose paper beats rock`);
-} 
-else if(humanChoice === "PAPER" && computerChoice === "ROCK") {
-  humanScore++
-  console.log(`You win paper beats rock`);
-} 
-else if (humanChoice === "PAPER" && computerChoice === "SCISSORS") {
-  computerScore++
-console.log(`You lose scissors beats paper`);
-} else if (humanChoice === "SCISSORS" && computerChoice === "PAPER") {
-  humanScore++
-console.log(`You win scissors beats paper`);
+  computerScore++;
+  console.log(`YOU lose! ${computerChoice} beats ${humanChoice}`);
+  
 }
-else if (humanChoice === "ROCK" && computerChoice === "SCISSORS") {
-  humanScore++
-  console.log(`You win rock beats scissors`);
 }
-else if (humanChoice === "SCISSORS" && computerChoice === "ROCK") {
-  computerScore++
-console.log(`You lose rock beats scissors`);
-}
-else {
-  return "It's a draw";
-}
-const computerSelection = getComputerChoice();
-const humanSelection = getHumanChoice();
-}
+const humanSelection = prompt(`ENTER: ROCK PAPER OR SCISSORS`);
+  const computerSelection = getComputerChoice();
 
-console.log(playRound(humanSelection, computerSelection));
+console.log(playRound("ROCK", "PAPER"));
